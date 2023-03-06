e1d5722409471413ce91b1b9c488e2756461d555
Nadia <ivashchenko.nadia@gmail.com>
2023-03-06 17:44:52 +0200

    Initial Commit

---
+++ Pr1_List_UK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pr1_List_UK
+//
+//  Created by Students on 06.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
